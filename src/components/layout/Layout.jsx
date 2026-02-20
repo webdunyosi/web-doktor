d76e49e1f7e906c17921a3c37345b51675bbe06c
Alimardon Toshpulatov <alimardontoshpulatov1978@gmail.com>
2026-02-20 17:12:57 +0500

    added header

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,3 +1,4 @@
+import Header from './Header';
 import Sidebar from './Sidebar';
 
 export default function Layout({ children }) {
@@ -5,6 +6,7 @@ export default function Layout({ children }) {
     <div className="flex min-h-screen bg-gray-50">
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
+        <Header />
         {children}
       </div>
     </div>
